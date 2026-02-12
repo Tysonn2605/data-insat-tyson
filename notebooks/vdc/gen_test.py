c0958e1377a8cfe611b83c9ed6eacd3ec1789e71
Tyson Pham <phamthaisonbin@gmail.com>
2026-02-12 23:44:57 +0000

    vdc

---
+++ notebooks/vdc/gen_test.py
@@ -0,0 +1,18 @@
+import random as rd
+rd.seed(5)
+
+n = 20
+limX = (0, 100)
+limY = (0, 100)
+city_pos = []
+for _ in range(n):
+    c = rd.randint(limX[0], limX[1]), rd.randint(limY[0], limY[1])
+    city_pos.append(c)
+
+with open("data.txt", "w", encoding="utf-8") as f:
+    for x in city_pos:
+        f.write(f"{x[0]}")
+        f.write(",")
+        f.write(f"{x[1]}")
+        f.write("\n")
+
